a2ad69b1b57e9dcebf0b71d56ec0996c76ea83fd
Ganesh1234-coder <63443601+Ganesh1234-coder@users.noreply.github.com>
2021-03-01 20:45:29 +0530

    Update Test.java

--- src/com/harshidhee/test/Test.java
+++ src/com/harshidhee/test/Test.java
@@ -3,7 +3,8 @@ package com.harshidhee.test;
 public class Test {
 
 	public static void main(String[] args) {
-	     System.out.println("My name is ganesh");		
+	     System.out.println("My name is ganesh");
+		System.out.println("My name is Ramesh");
 	}
 
 }
