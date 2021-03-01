35f194cd8786126856e3fbfdd93415be4bbdeb27
Ganesh1234-coder <63443601+Ganesh1234-coder@users.noreply.github.com>
2021-03-01 21:19:38 +0530

    commite  soe

--- src/com/harshidhee/test/Test.java
+++ src/com/harshidhee/test/Test.java
@@ -6,7 +6,7 @@ public class Test {
 	     System.out.println("My name is ganesh");
 		System.out.println("My name is Ramesh");
 		System.out.println("This is testing purpose only");
-		System.out.println("This is send to sts");
+		System.out.println("This is send to sts"+"not send any link");
 	}
 
 }
